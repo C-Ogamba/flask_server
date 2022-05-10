45f92a93c8aaf56fe5cd6825f2aa015969ca304c
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-10 23:38:26 +0300

    Dev: install bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
+from ensurepip import bootstrap
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 def createapp(config_class=Config):
     """factory function"""
 
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    bootstrap.init__app(app)
 
     """registering blueprints"""
     from app.main import main as main_bp
